048de01f6ec5f8a4267f9d8b9bbd8ebf2dbbe44e
AryanTy20 <devty007@gmail.com>
2022-07-01 00:22:53 +0530

    twaeks

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ const submitForm = () => {
   const title = document.querySelector(".search-input").value;
   if (title.length == 0) return;
   const str2 = title.charAt(0).toUpperCase() + title.slice(1);
-  const url = `http://localhost:5000/search?title=${str2}`;
+  const url = `https://flixnation.herokuapp.com/search?title=${str2}`;
   window.open(url, "_self");
 };
 const submitbtn = document.querySelector(".search-submit");
